d3613b02673df809b39d345b33e0c6f4dbe2912e
RRudi <maboitemail.romeu@gmail.com>
2016-11-25 18:33:36 +0100

    Modification de la lecture de la carte

--- app/src/main/java/com/beinfinity/activity/AccueilActivity.java
+++ app/src/main/java/com/beinfinity/activity/AccueilActivity.java
@@ -1,10 +1,14 @@
 package com.beinfinity.activity;
 
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.MifareClassic;
@@ -13,8 +17,14 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.acs.smartcard.Features;
+import com.acs.smartcard.PinModify;
+import com.acs.smartcard.PinVerify;
+import com.acs.smartcard.ReadKeyOption;
+import com.acs.smartcard.Reader;
 import com.beinfinity.R;
 
 import com.beinfinity.database.DbContract;
@@ -22,8 +32,10 @@ import com.beinfinity.database.DbHelper;
 import com.beinfinity.tools.Http;
 import com.beinfinity.tools.ProgressView;
 
+import java.io.Console;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class AccueilActivity extends AppCompatActivity {
 
@@ -45,6 +57,112 @@ public class AccueilActivity extends AppCompatActivity {
     private HashMap<String, String> parameters;
     private String idCard;
 
+
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private UsbManager mManager;
+    private Reader mReader;
+    private PendingIntent mPermissionIntent;
+    private Features mFeatures = new Features();
+    private PinVerify mPinVerify = new PinVerify();
+    private PinModify mPinModify = new PinModify();
+    private ReadKeyOption mReadKeyOption = new ReadKeyOption();
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        public void onReceive(Context context, Intent intent) {
+
+            String action = intent.getAction();
+            Toast.makeText(getApplicationContext(), "BroadcastReceiver action : " + action, Toast.LENGTH_LONG).show();
+
+            if (ACTION_USB_PERMISSION.equals(action))
+
+                synchronized (this) {
+
+                    UsbDevice device = intent
+                            .getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                    if (intent.getBooleanExtra(
+                            UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+
+                        if (device != null) {
+
+                            // Open reader
+                            Toast.makeText(getApplicationContext(), "open reader", Toast.LENGTH_SHORT);
+                            new OpenTask().execute(device);
+                        }
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "permissions denied", Toast.LENGTH_SHORT);
+                    }
+                }
+            else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+
+                synchronized (this) {
+
+                    UsbDevice device = intent
+                            .getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                    if (device != null && device.equals(mReader.getDevice())) {
+                        new CloseTask().execute();
+                    }
+                }
+            }
+        }
+    };
+
+
+    private class OpenTask extends AsyncTask<UsbDevice, Void, Exception> {
+
+        @Override
+        protected Exception doInBackground(UsbDevice... params) {
+
+            Exception result = null;
+
+            try {
+                Toast.makeText(getApplicationContext(), "OPEN", Toast.LENGTH_LONG).show();
+                mReader.open(params[0]);
+
+            } catch (Exception e) {
+
+                result = e;
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Exception result) {
+
+            if (result != null) {
+                Toast.makeText(getApplicationContext(), "Reader name: " + result.toString(), Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(getApplicationContext(), "Reader name: " + mReader.getReaderName(), Toast.LENGTH_SHORT).show();
+                int numSlots = mReader.getNumSlots();
+                Toast.makeText(getApplicationContext(), "Number of slots: " + numSlots, Toast.LENGTH_SHORT).show();
+                // Remove all control codes
+                mFeatures.clear();
+            }
+        }
+    }
+
+
+    private class CloseTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            mReader.close();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+        }
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,57 +177,104 @@ public class AccueilActivity extends AppCompatActivity {
 
         progressView = new ProgressView(mAccueilFormView, mProgressView);
 
-        this.mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-        this.pendingIntent = PendingIntent.getActivity(
-                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-
-        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
-        try {
-            ndef.addDataType("*/*");
-        } catch (IntentFilter.MalformedMimeTypeException e) {
-            throw new RuntimeException("fail", e);
-        }
-
-        this.intentFiltersArray = new IntentFilter[]{ndef,};
-        this.techListsArray = new String[][]{new String[]{
-                MifareClassic.class.getName()
-        }};
+//        this.mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+//
+//        this.pendingIntent = PendingIntent.getActivity(
+//                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+//
+//        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+//        try {
+//            ndef.addDataType("*/*");
+//        } catch (IntentFilter.MalformedMimeTypeException e) {
+//            throw new RuntimeException("fail", e);
+//        }
+//
+//        this.intentFiltersArray = new IntentFilter[]{ndef,};
+//        this.techListsArray = new String[][]{new String[]{
+//                MifareClassic.class.getName()
+//        }};
 
         this.GetDataFromDb();
+
+        this.InitUSB();
+
+        ImageButton i = (ImageButton) findViewById(R.id.imageButton2);
+        i.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                
+                String deviceName = "";
+                for (UsbDevice device : mManager.getDeviceList().values()) {
+                    if (mReader.isSupported(device)) {
+                        deviceName = device.getDeviceName();
+                    }
+                }
+
+                if (deviceName != null) {
+
+                    // For each device
+                    for (UsbDevice device : mManager.getDeviceList().values()) {
+
+                        // If device name is found
+                        if (deviceName.equals(device.getDeviceName())) {
+
+                            // Request permission
+                            mManager.requestPermission(device,
+                                    mPermissionIntent);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
+        Toast.makeText(getApplicationContext(), "Intent action : " + intent.getAction(), Toast.LENGTH_LONG).show();
         super.onNewIntent(intent);
-        if (intent != null && NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
-            Parcelable[] rawMessages =
-                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            if (rawMessages != null) {
-                NdefMessage[] messages = new NdefMessage[rawMessages.length];
-                for (int i = 0; i < rawMessages.length; i++) {
-                    messages[i] = (NdefMessage) rawMessages[i];
-                }
 
-                if (messages.length > 0) {
-                    // TODO: Faire vérification  suppl si nécessaire
-                    String message = new String(messages[0].getRecords()[0].getPayload());
-                    this.checkID(message);
-                }
-            }
-        }
+//        if (intent != null && NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+//            Parcelable[] rawMessages =
+//                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+//            if (rawMessages != null) {
+//                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+//                for (int i = 0; i < rawMessages.length; i++) {
+//                    messages[i] = (NdefMessage) rawMessages[i];
+//                }
+//
+//                if (messages.length > 0) {
+//                    // TODO: Faire vérification  suppl si nécessaire
+//                    String message = new String(messages[0].getRecords()[0].getPayload());
+//                    this.checkID(message);
+//                }
+//            }
+//        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        this.mNfcAdapter.enableForegroundDispatch(this, this.pendingIntent, this.intentFiltersArray, this.techListsArray);
-    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        this.mNfcAdapter.enableForegroundDispatch(this, this.pendingIntent, this.intentFiltersArray, this.techListsArray);
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        this.mNfcAdapter.disableForegroundDispatch(this);
+//    }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        this.mNfcAdapter.disableForegroundDispatch(this);
+    protected void onDestroy() {
+
+        // Close reader
+        mReader.close();
+
+        // Unregister receiver
+        unregisterReceiver(mReceiver);
+
+        super.onDestroy();
     }
 
     public void goToParameters(View view) {
@@ -118,7 +283,31 @@ public class AccueilActivity extends AppCompatActivity {
     }
 
     public void checkID(View view) {
-        this.checkID("12345678");
+        // this.checkID("1");
+
+
+        String deviceName = "";
+        for (UsbDevice device : mManager.getDeviceList().values()) {
+            if (mReader.isSupported(device)) {
+                deviceName = device.getDeviceName();
+            }
+        }
+
+        if (deviceName != null) {
+
+            // For each device
+            for (UsbDevice device : mManager.getDeviceList().values()) {
+
+                // If device name is found
+                if (deviceName.equals(device.getDeviceName())) {
+
+                    // Request permission
+                    mManager.requestPermission(device,
+                            mPermissionIntent);
+                    break;
+                }
+            }
+        }
     }
 
     private void checkID(String id) {
@@ -175,6 +364,109 @@ public class AccueilActivity extends AppCompatActivity {
             this.reason = response;
             return false;
         }
+    }
+
+    private void InitUSB() {
+
+        // Get USB manager
+        mManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        // Initialize reader
+        mReader = new Reader(mManager);
+        mReader.setOnStateChangeListener(new Reader.OnStateChangeListener() {
+
+            @Override
+            public void onStateChange(int slotNum, int prevState, int currState) {
+
+                if (prevState < Reader.CARD_UNKNOWN
+                        || prevState > Reader.CARD_SPECIFIC) {
+                    prevState = Reader.CARD_UNKNOWN;
+                }
+
+                if (currState < Reader.CARD_UNKNOWN
+                        || currState > Reader.CARD_SPECIFIC) {
+                    currState = Reader.CARD_UNKNOWN;
+                }
+                Toast.makeText(getApplicationContext(), "State - pre: " + prevState + " curr: " + currState, Toast.LENGTH_LONG).show();
+
+                // Create output string
+                //final String outputString = "Slot " + slotNum + ": "
+                //       + stateStrings[prevState] + " -> "
+                //       + stateStrings[currState];
+
+                // Show output
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        //logMsg(outputString);
+                        Toast.makeText(getApplicationContext(), "RUN THREAD", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        // Register receiver for USB permission
+        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
+                ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(mReceiver, filter);
+
+        // PIN verification command (ACOS3)
+        byte[] pinVerifyData = {(byte) 0x80, 0x20, 0x06, 0x00, 0x08,
+                (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+                (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+
+        // Initialize PIN verify structure (ACOS3)
+        mPinVerify.setTimeOut(0);
+        mPinVerify.setTimeOut2(0);
+        mPinVerify.setFormatString(0);
+        mPinVerify.setPinBlockString(0x08);
+        mPinVerify.setPinLengthFormat(0);
+        mPinVerify.setPinMaxExtraDigit(0x0408);
+        mPinVerify.setEntryValidationCondition(0x03);
+        mPinVerify.setNumberMessage(0x01);
+        mPinVerify.setLangId(0x0409);
+        mPinVerify.setMsgIndex(0);
+        mPinVerify.setTeoPrologue(0, 0);
+        mPinVerify.setTeoPrologue(1, 0);
+        mPinVerify.setTeoPrologue(2, 0);
+        mPinVerify.setData(pinVerifyData, pinVerifyData.length);
+
+        // PIN modification command (ACOS3)
+        byte[] pinModifyData = {(byte) 0x80, 0x24, 0x00, 0x00, 0x08,
+                (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+                (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+
+        // Initialize PIN modify structure (ACOS3)
+        mPinModify.setTimeOut(0);
+        mPinModify.setTimeOut2(0);
+        mPinModify.setFormatString(0);
+        mPinModify.setPinBlockString(0x08);
+        mPinModify.setPinLengthFormat(0);
+        mPinModify.setInsertionOffsetOld(0);
+        mPinModify.setInsertionOffsetNew(0);
+        mPinModify.setPinMaxExtraDigit(0x0408);
+        mPinModify.setConfirmPin(0x01);
+        mPinModify.setEntryValidationCondition(0x03);
+        mPinModify.setNumberMessage(0x02);
+        mPinModify.setLangId(0x0409);
+        mPinModify.setMsgIndex1(0);
+        mPinModify.setMsgIndex2(0x01);
+        mPinModify.setMsgIndex3(0);
+        mPinModify.setTeoPrologue(0, 0);
+        mPinModify.setTeoPrologue(1, 0);
+        mPinModify.setTeoPrologue(2, 0);
+        mPinModify.setData(pinModifyData, pinModifyData.length);
+
+        // Initialize read key option
+        mReadKeyOption.setTimeOut(0);
+        mReadKeyOption.setPinMaxExtraDigit(0x0408);
+        mReadKeyOption.setKeyReturnCondition(0x01);
+        mReadKeyOption.setEchoLcdStartPosition(0);
+        mReadKeyOption.setEchoLcdMode(0x01);
     }
 
     public class UserAuthTask extends AsyncTask<Void, Void, Boolean> {
